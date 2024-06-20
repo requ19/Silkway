f254344d3822456e2cb4f65f75e7d9e36bd16e26
WhiteShark03 <altykeev.temirlan99@gmail.com>
2024-06-20 11:57:09 +0600

    partners part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Home from "./pages/Home";
 import reset from './assets/styles/reset.module.scss'
+import Partners from "./pages/Partners"
+
 function App() {
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <Partners/>
     </>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import stylsess from './index.module.scss' 
+import styles from './index.module.scss' 
 import Header from '../../components/Header'
 const Home = () => {
   return (
