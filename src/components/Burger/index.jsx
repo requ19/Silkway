e8517e1c06d116e38046ed1005a6deda5312ab84
requ19 <azizmailcom1910@gmail.com>
2024-06-24 14:39:39 +0600

    header

--- src/components/Burger/index.jsx
+++ src/components/Burger/index.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import styles from './index.module.scss';
 
-const Burger = () => {
+const Burger = ({ setActive, active }) => {
   const [isActive, setIsActive] = useState(false);
 
-  const toggleActive = () => {
+  const handleClick = () => {
     setIsActive(!isActive);
+    setActive(!active);
   };
 
   return (
     <div
-      className={`${styles.button} ${isActive ? styles.active : ''}`} 
-      onClick={toggleActive}
+      className={`${styles.button} ${isActive ? styles.active : ''}`}
+      onClick={handleClick}
     >
       <div className={styles.icon}>
         <span></span>
       </div>
-      
     </div>
   );
 };
